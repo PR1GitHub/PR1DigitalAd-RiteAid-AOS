1906417f5755b7f413783928287332c2e29f9a8f
Tarun-eware <tkala@efficiencyware.com>
2024-08-22 15:58:08 +0530

    Two communication approach update

--- app/src/main/java/com/purered/pr1digitalad_riteaid_aos/MainActivity.kt
+++ app/src/main/java/com/purered/pr1digitalad_riteaid_aos/MainActivity.kt
@@ -10,14 +10,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.purered.pr1digitalad_riteaid_aos.ui.theme.PR1DigitalAdRiteAidAOSTheme
 
 import android.view.View
-import org.json.JSONArray
 import com.purered.pr1digitalad.DigitalAd
 import com.purered.pr1digitalad.DigitalAdInput
+import com.purered.pr1digitalad.ActionPayload
+import org.json.JSONObject
+import org.json.JSONArray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+ var digitalAd: DigitalAd? = null;
 
 
         val cartItems = listOf<Map<String, Any>>(
@@ -26,9 +30,8 @@ class MainActivity : ComponentActivity() {
         );
         val clippedCoupons = listOf<Map<String, Any>>(
             mapOf( "couponId" to "70f6af3f-1b6d-452e-b699-d62020b8c7e4",  "id" to "8ea69008-aa35-4d2b-a075-9012b45e835f"  ),
-
-            //  mapOf( "couponId" to "70af9ac4-50e8-499b-b9e4-7f2dd4b10351",  "id" to "e61f961d-abc9-4a33-a88d-23ac36d83756"  )
         );
+
         val options:DigitalAdInput =   DigitalAdInput(  "pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp3",
             "10074",
             "",
@@ -38,21 +41,63 @@ class MainActivity : ComponentActivity() {
             ""
 
         ){
-                actionType:String,payload: JSONArray ->
-            when (actionType) {
-                "onClipCoupon" -> {
-                    // Perform action for onClipCoupon
-                    println("Performing action for onClipCoupon with payload: $payload")
-                }
-                "onSelectStore" -> {
-                    // Perform action for onUnclipCoupon
-                    println("Performing action for onUnclipCoupon")
-                }
-                else -> {
-                    // Default case or handle other cases
-                    println("Unknown case: $actionType with object: $payload")
-                }
-            }
+                payload: ActionPayload ->
+
+
+            println("Call Back Handler: ${payload.actionName} with object: $payload")
+
+
+
+                    when (payload.actionName) {
+                        "onClipCoupon" -> {
+
+                            // Clipped coupons will be accessed from the payload.value
+                            println("Performing action for onClipCoupon with payload: ${payload.value}")
+
+
+
+                            if(digitalAd != null){
+                                //eventCallback
+
+                                payload.status = "success"; // very important to set the status to success or error and to Show the message
+
+                              //   Update the payload value with the new clipped coupons if needed
+                                payload.value = listOf<Map<String, Any>>(
+                                    mapOf( "couponId" to "70f6af3f-1b6d-452e-b699-d62020b8c7e4",  "id" to "8ea69008-aa35-4d2b-a075-9012b45e835f"  ),
+                                );
+
+
+                                digitalAd!!.dispatch(payload)
+
+
+                                //Or
+//                                digitalAd!!.dispatch(
+//                                    ActionPayload(
+//                                        status = "success",
+//                                        value =  listOf<Map<String, Any>>(
+//                                            mapOf( "couponId" to "70f6af3f-1b6d-452e-b699-d62020b8c7e4",  "id" to "8ea69008-aa35-4d2b-a075-9012b45e835f"  ),
+//                                        ),
+//                                        actionContext = payload.actionContext,
+//                                        actionName = payload.actionName
+//
+//                                    ))
+
+
+
+                            }
+
+
+
+                        }
+                        "onUnclipCoupon" -> {
+                            // Perform action for onUnclipCoupon
+                            println("Performing action for onUnclipCoupon")
+                        }
+                        else -> {
+                            // Default case or handle other cases
+                            println("Unknown case: $payload.actionName with object: $payload")
+                        }
+                    }
         }
 
 
@@ -62,7 +107,7 @@ class MainActivity : ComponentActivity() {
 
 
         // Initialise DigitalAd with the required options parameter of type DigitalAdInput
-        val digitalAd = DigitalAd(this, options)
+          digitalAd = DigitalAd(this, options)
 
         val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT )
         digitalAd.updateLayoutParams(layoutParams)
@@ -71,22 +116,16 @@ class MainActivity : ComponentActivity() {
         val adViewRef = digitalAd.viewRef;
         findViewById<LinearLayout>(R.id.container).addView(adViewRef)
 
+
+
         val btnValidate: View = findViewById(R.id.btnvalidate)
 
 
         btnValidate.setOnClickListener { view: View ->
-//            val payload = JSONArray()
-//            var couponOne = JSONObject()
-//            couponOne.put("couponId" , "70af9ac4-50e8-499b-b9e4-7f2dd4b10351")
-//            couponOne.put("id" , "e61f961d-abc9-4a33-a88d-23ac36d83756")
-//
-//            payload.put(0, couponOne)
-//            // Dispatch Digital Ad Methods needs action name, action payload
-//            digitalAd.dispatch("clipCoupon", payload)
-            //     digitalAd.dispatch("setStore", "01301")
 
-            digitalAd.dispatch("setClippedCoupons", "01301")
 
+            //  To update the clipped coupons, call the dispatch method with the action name and the new clipped coupons.
+            digitalAd.dispatch(ActionPayload( actionName = "setClippedCoupons", value = clippedCoupons))
 
         }
 
